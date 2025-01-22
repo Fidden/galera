2b2f77eab18e7f1843b0edfb57d667f594cee67c
fidden <sahalisa127@gmail.com>
2025-01-22 17:53:49 +0300

    inti.

--- projects/backend/docker-entrypoint.sh
+++ projects/backend/docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+chmod -R ugo+rw /.composer
+chown -R $USER:www-data /var/www/html/storage/
+chown -R $USER:www-data /var/www/html/bootstrap/cache/
+chmod -R 775 /var/www/html/storage/
+chmod -R 775 /var/www/html/bootstrap/cache/
+
 php artisan migrate --force
 
 php artisan app:products
